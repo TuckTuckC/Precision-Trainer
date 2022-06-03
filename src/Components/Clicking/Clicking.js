fa7aad6597f9c4ba13ceb41e3f5353503bdbbeb0
Tucker Craig <tuckermcraig@gmail.com>
2022-06-03 18:33:35 -0400

    first 5 targets spawn

--- src/Components/Clicking/Clicking.js
+++ src/Components/Clicking/Clicking.js
@@ -10,6 +10,14 @@ function Clicking() {
 
     // State
     const [score, setScore] = useState(0)
+    const [targetArray, setTargetArray] = useState([0, 1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15, 16, 17, 18, 19, 20, 21, 22, 23, 24, 25, 26, 27, 28, 29, 30, 31, 32, 33, 34, 35, 36, 37, 38, 39, 40, 41, 42, 43, 44, 45, 46, 47, 48, 49, 50, 51, 52, 53, 54, 55, 56, 57, 58, 59, 60, 61, 62, 63, 64, 65, 66, 67, 68, 69])
+    const [random, setRandom] = useState(
+        Array.from(
+            {length: 5}, () => Math.floor(Math.random() * 69)
+        )
+    );
+
+    console.log(random);
 
     // STRETCH GOAL: Dynamic playboard sizes
     // const [playHeight, setPlayHeight] = useState(15)
@@ -21,76 +29,9 @@ function Clicking() {
         <div className="container">
             <p className="score">Score: {score}</p>
             <div className="playBoard">
-                <Target />
-                <Target />
-                <Target />
-                <Target />
-                <Target />
-                <Target />
-                <Target />
-                <Target />
-                <Target />
-                <Target />
-                <Target />
-                <Target />
-                <Target />
-                <Target />
-                <Target />
-                <Target />
-                <Target />
-                <Target />
-                <Target />
-                <Target />
-                <Target />
-                <Target />
-                <Target />
-                <Target />
-                <Target />
-                <Target />
-                <Target />
-                <Target />
-                <Target />
-                <Target />
-                <Target />
-                <Target />
-                <Target />
-                <Target />
-                <Target />
-                <Target />
-                <Target />
-                <Target />
-                <Target />
-                <Target />
-                <Target />
-                <Target />
-                <Target />
-                <Target />
-                <Target />
-                <Target />
-                <Target />
-                <Target />
-                <Target />
-                <Target />
-                <Target />
-                <Target />
-                <Target />
-                <Target />
-                <Target />
-                <Target />
-                <Target />
-                <Target />
-                <Target />
-                <Target />
-                <Target />
-                <Target />
-                <Target />
-                <Target />
-                <Target />
-                <Target />
-                <Target />
-                <Target />
-                <Target />
-                <Target />
+                {targetArray.map((key, index) => (
+                    <Target random={random} key={key} index={index}/>
+                ))}
             </div>
         </div>
     </div>
--- src/Components/Clicking/Target/Target.js
+++ src/Components/Clicking/Target/Target.js
@@ -1,10 +1,28 @@
-import React from 'react'
+import React, { useState, useEffect } from 'react'
 
 import './Target.css';
 
-function Target() {
+function Target({ index, random }) {
+
+    const [active, setActive] = useState(false)
+
+    const bot = document.getElementsByClassName(`target${index}`);
+
+    console.log(random, index);
+
+    const firstSpawn = () => {
+        if (random.includes(index)) {
+            setActive(true);
+        }
+    }
+
+    useEffect(() => {
+        firstSpawn()
+    })
+
+
   return (
-    <div className='target'>Target</div>
+    <div className={active ? `active target target${index}` : `target target${index}`}>{index}</div>
   )
 }
 
